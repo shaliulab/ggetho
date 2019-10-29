729d8c012e7c761fcd4b68b176380c49da837d06
Antonio <ntoniohu@gmail.com>
2019-10-29 23:26:08 +0100

    Define ggetho_preprocess, a function that performs the same data preprocessing ggetho does

--- R/ggetho.R
+++ R/ggetho.R
@@ -77,137 +77,22 @@ ggetho <- function(data,
                     time_offset = 0,
                     multiplot = NULL, # 1
                     multiplot_period = hours(24),
-                    cache = '/tmp/',
                     ...){
 
-  # trick to avoid NOTES from R CMD check:
-  x_off = x_name = y =  . = NULL
+  preprocess_result <- ggetho_preprocess(data, mapping, summary_FUN,
+                                         summary_time_window,
+                                         time_wrap,
+                                         time_offset,
+                                         multiplot,
+                                         multiplot_period)
 
-  dir.create(cache, showWarnings = FALSE)
-
-  ## Handle the time_offset and time_wrap args
-  ## In case time_offset is greater than the time_wrap
-  ## it has to be set then to its modulus
-  if(time_offset != 0 & is.null(time_wrap))
-    warning("Time offset only relevant when using time_wrap.
-             Ignoring argument")
-  else
-    time_offset <- time_offset %% time_wrap
-
-  #todo check argument types!!
-  if(!is.null(multiplot)){
-  if(!is.null(time_wrap))
-    stop("Cannot use time wrapping on a multiplot!") #todo
-  if(!multiplot %in% 2:10000)
-    stop("multiplot must be an integer >1, typically 2, for double plotting")
-  }
-
-  ## Handle the possible combination of aesthetics variables
-  ## Get a character vector containing the mappings so as to
-  ## implement the logic that fine tunes the plot
-  ## For example, the x axis is set as a function of what var
-  ## is mapped to it. ggetho sets the scale accordingly to hours, days, etc
-  #mapping_list <- as.list(as.character(mapping))
-  mapping_list <- make_labels(mapping)
-  aes_names <- names(mapping_list)
-  has_colour = "colour" %in% aes_names
-  has_fill = "fill" %in% aes_names
-
-  # if has only colour Xor fill defined
-  if( xor(has_fill, has_colour)){
-     col = c(mapping_list$fill, mapping_list$colour)[[1]]
-     mapping_list$fill <- col
-     mapping_list$colour <- col
-  }
+  sdt <- preprocess_result$dt
+  mapping <- preprocess_result$mapping
+  scale_x_FUN <- preprocess_result$scale_x_fun
+  discrete_y <- preprocess_result$discrete_y
+  time_offset <- preprocess_result$time_offset
 
 
-  # by default, t is the dimension mapped to X
-  if(!"x" %in% aes_names)
-    mapping_list$x = "t"
-
-  # by default y is not discrete and is the variable of interest
-  # (unless z is given), in which case y is discrete
-  # and z becomes the variable of interest
-  # You cannot provide both
-  x_name <- mapping_list$x
-  discrete_y <- FALSE
-  if("z" %in% aes_names){
-    var_of_interest = mapping_list$z
-    discrete_y <- TRUE
-  }
-  else if("y" %in% aes_names)
-    var_of_interest = mapping_list$y
-  else
-    stop("Either `y` or `z` should be provided as variable of interest")
-
-  ## Apply the summarizing function to the data using a window size
-  ## of summary_time_window (default 30 mins)
-  ## if time_wrap, then datapoints lying in the same fraction of the 24h
-  ## are averaged, which results in a plot showing the average cycle
-  ## The summarizing function is by default mean because it computes
-  ## the fraction of TRUES (asleep) in the window
-  sdt <- fslbehavr::bin_apply_all(data,
-                               var_of_interest,
-                               x = x_name,
-                               x_bin_length = summary_time_window,
-                               wrap_x_by = time_wrap,
-                               FUN = summary_FUN)
-
-  # Join data and metadata together so ggplot2 has access to everything
-  sdt <- rejoin(sdt)
-  # when no `y` is provided, the default is to have a
-  # discrete/factor axis with individuals as rows
-
-  if(is.null(multiplot)){
-    if(!"y" %in% aes_names){
-      # todo check those columns exist
-      mapping_list$y = "id"
-    }
-  }
-  else{
-    if("y" %in% aes_names){
-      stop("Cannot use y AND multiplot.
-            When multiploting, the y axis is used for consecutive periods,
-            the variable of interest should be on the z axis")
-    }
-    sdt <- make_multiplot(sdt, multiplot, multiplot_period, summary_time_window)
-    mapping_list$y = "period"
-    discrete_y <- TRUE
-  }
-
-  if(!is.null(time_wrap)){
-    sdt[, x_off := eval(parse(text = x_name)) ]
-    sdt[, x_off := ((x_off + time_offset) %% time_wrap ) - time_offset]
-    sdt[, x_name] <- sdt[, x_off]
-    sdt[, x_off := NULL]
-  }
-    #sdt[,,.SD,keyby=c("id", "x_name")]
-
-  # Save the summarized data if a cache is given
-  # if(!is.null(cache)) {
-  #   # browser()
-  #   tryCatch(expr = {
-  #   output_table <- copy(sdt)
-  #   if("file_info" %in% colnames(output_table)) {
-  #     output_table[, file_info := unlist(lapply(file_info, function(x) x$path))]
-  #   }
-  #   fileName <- file.path(cache, "dt_bin.csv")
-  #   fwrite(x = output_table, file = fileName)
-  #
-  #   write.table(x = sdt, file = file.path(cache, "dt_bin.csv"), sep = ",", quote = F, row.names = F, col.names = T)
-  #   }, error = browser())
-  # }
-
-  scale_x_FUN <- auto_x_time_scale(sdt[[mapping_list$x]])
-  mapping_list <- lapply(mapping_list,
-                         function(x){
-                                      if(x %in% colnames(sdt))
-                                        paste0("`", x, "`")
-                                      else
-                                        x
-                                    })
-
-  mapping = do.call(aes_string, mapping_list)
   out <- ggplot(sdt, mapping,...)
   # add some vertical margin  to the plot when needed, this is to show
   # LD annotations
@@ -222,17 +107,10 @@ ggetho <- function(data,
   }
 
   if(!is.null(time_wrap))
-    return( out + scale_x_FUN(limits=c(- time_offset, time_wrap- time_offset)))
+    return( out + scale_x_FUN(limits=c(- time_offset, time_wrap - time_offset)))
 
   plot <- out + scale_x_FUN()
-  if (!is.null(cache))
-    return(list(
-      plot = plot,
-      sdt = NA
-      # sdt = output_table
-      ))
-    else
-    return(plot)
+  return(plot)
 }
 
 
